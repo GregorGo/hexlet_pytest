5cee4d4beff492a7c2042ed5020ed2ddc2584fbb
Grisha Igoshkin <mr.igoshkin@inbox.ru>
2024-05-27 22:43:08 +0300

    add wrong test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,7 @@
 from hexlet_pytest.example import reverse
 
 def test_reverse():
-    assert reverse('Hexlet') == 'telxeH'
+    assert reverse('Hexlet') == 'teleH'
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
